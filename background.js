abac87fdfd497ffb23edebd566fade657dcaf4c0
jenessachap <jenessachap@gmail.com>
2021-03-31 20:16:02 -0700

    updates

--- background.js
+++ background.js
@@ -4,3 +4,9 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+const quotes = {
+  1: "You got this!!!",
+  2: "You are learning",
+  3: "You can do hard things",
+}
--- popup.js
+++ popup.js
@@ -1,5 +1,15 @@
-// Initialize butotn with users's prefered color
+// Initialize button with users's prefered color
+const quotes = {
+  0: "DO IT!!!",
+  1: "You got this!!!",
+  2: "You are learning",
+  3: "You can do hard things",
+}
+
 let changeColor = document.getElementById("changeColor");
+
+
+
 
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
@@ -9,12 +19,18 @@ chrome.storage.sync.get("color", ({ color }) => {
 changeColor.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
+
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: setPageBackgroundColor,
   });
 });
 
+changeColor.addEventListener('click',() => {
+    // if (i >= quotes.length) i = 0;
+    changeColor.innerHTML = quotes[Math.floor(Math.random() * Object.keys(quotes).length)];
+    console.log(Math.floor(Math.random() * quotes.length), quotes.length)
+})
 // The body of this function will be execuetd as a content script inside the
 // current page
 function setPageBackgroundColor() {
